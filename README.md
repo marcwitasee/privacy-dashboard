5b42a3d95ebbccef49e0c6c59a4e76223e5ede77
Marc Richardson <mtrichardson@uchicago.edu>
2021-05-10 14:23:14 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tech for Public Policy - Privacy Dashboard
+
+Contains code for scrapping data on organizations and agencies that have allegedly used Clearview AI's facial recognition software and code use to create visualizations with the scrapped data.
---
+++ web_scraper/buzzfeed_scaper.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import re
+import pandas as pd
+import time
+
+option = webdriver.ChromeOptions()
+option.add_argument(" — incognito")
+
+browser = webdriver.Chrome(
+    executable_path="/Users/criterionmarc/uchicago/spring2021/tech-for-policy/project/chromedriver",
+    options=option
+)
+
+data = []
+browser.get("https://datawrapper.dwcdn.net/VOOIE/16/")
+
+page_num = browser.find_element(By.CSS_SELECTOR, "div.pagination")
+current_page, total_pages = re.findall(r'\d+', page_num.text)
+
+while True:
+
+    table_rows = browser.find_elements(By.CSS_SELECTOR, "tr.css-11lloh5")
+
+    for row in table_rows:
+        values = []
+        cells = row.find_elements(By.TAG_NAME, "td")
+        for cell in cells:
+            values.append(cell.text)
+        data.append(values)
+
+    if int(current_page) == int(total_pages):
+        break
+
+    browser.find_element(By.CSS_SELECTOR, "button.next").click()
+    time.sleep(2)
+    page_num = browser.find_element(By.CSS_SELECTOR, "div.pagination")
+    current_page, _ = re.findall(r'\d+', page_num.text)
+
+df = pd.DataFrame(data)
+
+df.to_csv(
+    "/Users/criterionmarc/uchicago/spring2021/tech-for-policy/project/web_scraper/data/buzzfeed_cvai.csv",
+    index=False
+)
+
+browser.quit()
